298fa804da4fb67dbb94af4200136511a9ae9689
Dominic Tripodi <dtripodi93@gmail.com>
2022-08-09 17:44:23 -0400

    Updated Intermediate SQL Script Section

--- SQLScripts/Intermediate.sql
+++ SQLScripts/Intermediate.sql
@@ -2,250 +2,170 @@ USE DotNetCourseDatabase;
 GO
 
 SELECT  Users.UserId
-        , Users.FirstName
-        , Users.LastName
+        , Users.FirstName + ' ' + Users.LastName AS FullName
+        , Users.Email
         , Users.Gender
         , Users.Active
-  FROM  TutorialAppSchema.Users
- WHERE  Users.Active = 1;
+  FROM  TutorialAppSchema.Users AS Users
+ WHERE  Users.Active = 1
+ ORDER BY Users.UserId DESC;
 
 SELECT  Users.UserId
-        , Users.FirstName
-        , Users.LastName
+        , Users.FirstName + ' ' + Users.LastName AS FullName
+        , UserJobInfo.JobTitle
+        , UserJobInfo.Department
+        , UserSalary.Salary
+        , Users.Email
         , Users.Gender
         , Users.Active
-        , UserJobInfo.Department
-        , UserJobInfo.JobTitle
-        , UserSalary.Salary
-  FROM  TutorialAppSchema.Users
-      JOIN TutorialAppSchema.UserJobInfo
-          ON UserJobInfo.UserId = Users.UserId
-      JOIN TutorialAppSchema.UserSalary
+  FROM  TutorialAppSchema.Users AS Users
+      --INNER JOIN
+      JOIN TutorialAppSchema.UserSalary AS UserSalary
           ON UserSalary.UserId = Users.UserId
-             AND Users.Active = 1;
+      LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+          ON UserJobInfo.UserId = Users.UserId
+ WHERE  Users.Active = 1
+ ORDER BY Users.UserId DESC;
 
+-- DELETE FROM TutorialAppSchema.UserSalary WHERE UserId BETWEEN 250 AND 750 --501 Rows
+-- --When we use BETWEEN we also include the Lower and Upper Bound of the value we're checking
 SELECT  UserSalary.UserId
         , UserSalary.Salary
-  FROM  TutorialAppSchema.UserSalary
+  FROM  TutorialAppSchema.UserSalary AS UserSalary
  WHERE  EXISTS (
                    SELECT   *
-                     FROM   TutorialAppSchema.Users
-                    WHERE   Users.UserId = UserSalary.UserId
-                            AND Users.Active = 1
-               );
+                     FROM   TutorialAppSchema.UserJobInfo AS UserJobInfo
+                    WHERE   UserJobInfo.UserId = UserSalary.UserId
+               )
+        AND UserId <> 7;
 
-SELECT  Users.UserId
-        , Users.FirstName
-        , Users.LastName
-        , Users.Gender
-        , Users.Active
-        , UserJobInfo.Department
-        , UserJobInfo.JobTitle
-        , UserSalary.Salary
-  FROM  TutorialAppSchema.Users
-      JOIN TutorialAppSchema.UserJobInfo
-          ON UserJobInfo.UserId = Users.UserId
-      LEFT JOIN TutorialAppSchema.UserSalary
-          ON UserSalary.UserId = Users.UserId
-             AND Users.Active = 1;
-
-SELECT  UserSalary.UserId
-        , UserSalary.Salary
+SELECT  UserId
+        , Salary
   FROM  TutorialAppSchema.UserSalary
+-- UNION --Distinct /*Between the two queries*/
 UNION ALL
-SELECT  UserSalary.UserId
-        , UserSalary.Salary
+SELECT  UserId
+        , Salary
   FROM  TutorialAppSchema.UserSalary;
 
 SELECT  Users.UserId
-        , Users.FirstName
-        , Users.LastName
-        , Users.Gender
-        , Users.Active
-        , UserJobInfo.Department
+        , Users.FirstName + ' ' + Users.LastName AS FullName
         , UserJobInfo.JobTitle
+        , UserJobInfo.Department
         , UserSalary.Salary
-  FROM  TutorialAppSchema.Users
-      JOIN TutorialAppSchema.UserJobInfo
-          ON UserJobInfo.UserId = Users.UserId
-      LEFT JOIN (
-                    SELECT  UserSalary.UserId
-                            , UserSalary.Salary
-                      FROM  TutorialAppSchema.UserSalary
-                    UNION ALL
-                    SELECT  UserSalary.UserId
-                            , UserSalary.Salary
-                      FROM  TutorialAppSchema.UserSalary
-                ) UserSalary
-          ON UserSalary.UserId = Users.UserId
-             AND Users.Active = 1;
-
-SELECT  UserSalary.UserId
-        , UserSalary.Salary
-        , Users.UserId
-        , Users.FirstName
-        , Users.LastName
+        , Users.Email
         , Users.Gender
         , Users.Active
-  FROM  TutorialAppSchema.UserSalary
-      RIGHT JOIN TutorialAppSchema.Users
-          ON Users.UserId = UserSalary.UserId
-             AND Users.Active = 1;
-
---CREATE NONCLUSTERED INDEX ix_UserSalary_UserId ON TutorialAppSchema.UserSalary(UserId) INCLUDE(Salary)
-
---CREATE CLUSTERED INDEX cix_UserJobInfo_UserId ON TutorialAppSchema.UserJobInfo(UserId)
-SELECT  UserJobInfo.Department
-        , SUM (UserSalary.Salary)
-  FROM  TutorialAppSchema.Users
-      JOIN TutorialAppSchema.UserJobInfo
-          ON UserJobInfo.UserId = Users.UserId
-      JOIN TutorialAppSchema.UserSalary
+  FROM  TutorialAppSchema.Users AS Users
+      --INNER JOIN
+      JOIN TutorialAppSchema.UserSalary AS UserSalary
           ON UserSalary.UserId = Users.UserId
- GROUP BY UserJobInfo.Department;
-
-SELECT  UserJobInfo.JobTitle
-        , SUM (UserSalary.Salary)
-  FROM  TutorialAppSchema.Users
-      JOIN TutorialAppSchema.UserJobInfo
+      LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
           ON UserJobInfo.UserId = Users.UserId
-      JOIN TutorialAppSchema.UserSalary
-          ON UserSalary.UserId = Users.UserId
- GROUP BY UserJobInfo.JobTitle;
+ WHERE  Users.Active = 1
+ ORDER BY Users.UserId DESC;
 
-SELECT  UserJobInfo.Department
-        , SUM (UserSalary.Salary) SalarySum
-        , AVG (UserSalary.Salary) AS SalaryAverage
-        , MIN (UserSalary.Salary) AS SalaryMinimum
-        , MAX (UserSalary.Salary) AS SalaryMaximum
-        , STRING_AGG (UserSalary.UserId, ',') AS UsersInDepartment
-  FROM  TutorialAppSchema.Users
-      JOIN TutorialAppSchema.UserJobInfo
-          ON UserJobInfo.UserId = Users.UserId
-      JOIN TutorialAppSchema.UserSalary
-          ON UserSalary.UserId = Users.UserId
- GROUP BY UserJobInfo.Department;
+CREATE CLUSTERED INDEX cix_UserSalary_UserId
+    ON TutorialAppSchema.UserSalary (UserId);
 
-SELECT  CASE WHEN UserJobInfo.Department = 'Accounting' THEN 1 ELSE 0 END AS IsAccounting
-        , SUM (UserSalary.Salary) SalarySum
-        , AVG (UserSalary.Salary) AS SalaryAverage
-        , MIN (UserSalary.Salary) AS SalaryMinimum
-        , MAX (UserSalary.Salary) AS SalaryMaximum
-        , STRING_AGG (UserSalary.UserId, ',') AS UsersInDepartment
-  FROM  TutorialAppSchema.Users
-      JOIN TutorialAppSchema.UserJobInfo
-          ON UserJobInfo.UserId = Users.UserId
-      JOIN TutorialAppSchema.UserSalary
-          ON UserSalary.UserId = Users.UserId
- GROUP BY UserJobInfo.Department;
+CREATE NONCLUSTERED INDEX ix_UserJobInfo_JobTitle
+    ON TutorialAppSchema.UserJobInfo (JobTitle)
+    INCLUDE (Department);
 
---GROUP BY CASE WHEN UserJobInfo.Department = 'Accounting' THEN 1 ELSE 0 END;
-SELECT  UserJobInfo.JobTitle
-        , SUM (UserSalary.Salary) SalarySum
-        , AVG (UserSalary.Salary) AS SalaryAverage
-        , MIN (UserSalary.Salary) AS SalaryMinimum
-        , MAX (UserSalary.Salary) AS SalaryMaximum
-        , STRING_AGG (UserSalary.UserId, ',') AS UsersInJobTitle
-  FROM  TutorialAppSchema.Users
-      JOIN TutorialAppSchema.UserJobInfo
-          ON UserJobInfo.UserId = Users.UserId
-      JOIN TutorialAppSchema.UserSalary
+CREATE NONCLUSTERED INDEX fix_Users_Active
+    ON TutorialAppSchema.Users (active)
+    INCLUDE (Email, FirstName, LastName) --Also Includes UserId because it is our clustered Index 
+    WHERE active = 1;
+
+SELECT  ISNULL (UserJobInfo.Department, 'No Department Listed') AS Deparment
+        , SUM (UserSalary.Salary) AS Salary
+        , MIN (UserSalary.Salary) AS MinSalary
+        , MAX (UserSalary.Salary) AS MaxSalary
+        , AVG (UserSalary.Salary) AS AvgSalary
+        , COUNT (*) AS PeopleInDepartment
+        , STRING_AGG (Users.UserId, ', ') AS UserIds
+  FROM  TutorialAppSchema.Users AS Users
+      --INNER JOIN
+      JOIN TutorialAppSchema.UserSalary AS UserSalary
           ON UserSalary.UserId = Users.UserId
- GROUP BY UserJobInfo.JobTitle;
+      LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+          ON UserJobInfo.UserId = Users.UserId
+ WHERE  Users.Active = 1
+ GROUP BY UserJobInfo.Department
+ ORDER BY ISNULL (UserJobInfo.Department, 'No Department Listed') DESC;
 
 SELECT  Users.UserId
-        , Users.FirstName
-        , Users.LastName
+        , Users.FirstName + ' ' + Users.LastName AS FullName
+        , UserJobInfo.JobTitle
+        , UserJobInfo.Department
+        , DepartmentAverage.AvgSalary
+        , UserSalary.Salary
+        , Users.Email
         , Users.Gender
         , Users.Active
-        , UJI.Department
-        , UJI.JobTitle
-        , ISNULL (UserSalary.Salary, AvgSalaryInDepartment.AvgSalaryInDepartment) AS Salary
-        , CASE WHEN UserSalary.Salary IS NULL THEN 1 ELSE 0 END AS SalaryAssumed
-  FROM  TutorialAppSchema.Users
-      JOIN TutorialAppSchema.UserJobInfo AS UJI
-          ON UJI.UserId = Users.UserId
-      LEFT JOIN TutorialAppSchema.UserSalary
+  FROM  TutorialAppSchema.Users AS Users
+      --INNER JOIN
+      JOIN TutorialAppSchema.UserSalary AS UserSalary
           ON UserSalary.UserId = Users.UserId
-             AND Users.Active = 1
-      OUTER APPLY (
-                      SELECT    UJI2.Department
-                                , AVG (UserSalary.Salary) AS AvgSalaryInDepartment
-                        FROM    TutorialAppSchema.Users
-                            JOIN TutorialAppSchema.UserJobInfo UJI2
-                                ON UJI2.UserId = Users.UserId
-                            JOIN TutorialAppSchema.UserSalary
-                                ON UserSalary.UserId = Users.UserId
-                       WHERE UJI2.Department = UJI.Department
-                       GROUP BY UJI2.Department
-                  ) AS AvgSalaryInDepartment;
-
-SELECT  Users.UserId
-        , Users.FirstName
-        , Users.LastName
-        , Users.Gender
-        , Users.Active
-        , UJI.Department
-        , UJI.JobTitle
-        , ISNULL (UserSalary.Salary, AvgSalaryInDepartment.AvgSalaryInDepartment) AS Salary
-        , CASE WHEN UserSalary.Salary IS NULL THEN 1 ELSE 0 END AS SalaryAssumed
-  FROM  TutorialAppSchema.Users
-      JOIN TutorialAppSchema.UserJobInfo AS UJI
-          ON UJI.UserId = Users.UserId
-      LEFT JOIN TutorialAppSchema.UserSalary
-          ON UserSalary.UserId = Users.UserId
-             AND Users.Active = 1
-      CROSS APPLY (
-                      SELECT    UJI2.Department
-                                , AVG (UserSalary.Salary) AS AvgSalaryInDepartment
-                        FROM    TutorialAppSchema.Users
-                            JOIN TutorialAppSchema.UserJobInfo UJI2
-                                ON UJI2.UserId = Users.UserId
-                            JOIN TutorialAppSchema.UserSalary
-                                ON UserSalary.UserId = Users.UserId
-                       WHERE UJI2.Department = UJI.Department
-                             AND UJI2.Department = 'Accounting'
-                       GROUP BY UJI2.Department
-                  ) AS AvgSalaryInDepartment;
+      LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+          ON UserJobInfo.UserId = Users.UserId
+      -- OUTER APPLY ( -- Similar to LEFT JOIN
+      CROSS APPLY ( -- Similar to JOIN
+                      -- SELECT TOP 1 
+                      SELECT    ISNULL (UserJobInfo2.Department, 'No Department Listed') AS Deparment
+                                , AVG (UserSalary2.Salary) AS AvgSalary
+                        FROM    TutorialAppSchema.UserSalary AS UserSalary2
+                            LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo2
+                                ON UserJobInfo2.UserId = UserSalary2.UserId
+                       WHERE UserJobInfo2.Department = UserJobInfo.Department
+                       GROUP BY UserJobInfo2.Department
+                  ) AS DepartmentAverage
+ WHERE  Users.Active = 1
+ ORDER BY Users.UserId DESC;
 
 SELECT  GETDATE ();
 
-SELECT  DATEADD (DAY, 25, GETDATE ());
+SELECT  DATEADD (YEAR, -5, GETDATE ());
 
-SELECT  DATEADD (DAY, -25, GETDATE ());
+SELECT  DATEDIFF (MINUTE, DATEADD (YEAR, -5, GETDATE ()), GETDATE ());  -- Returns Positive
 
-SELECT  DATEDIFF (DAY, GETDATE (), DATEADD (DAY, -25, GETDATE ()));
+SELECT  DATEDIFF (MINUTE, GETDATE (), DATEADD (YEAR, -5, GETDATE ()));  -- Returns Negative
 
-ALTER TABLE TutorialAppSchema.UserSalary
-ADD AvgForDepartment DECIMAL(18, 4);
+ALTER TABLE TutorialAppSchema.UserSalary ADD AvgSalary DECIMAL(18, 4);
 
-ALTER TABLE TutorialAppSchema.UserSalary ADD AvgForJobTitle DECIMAL(18, 4);
+SELECT  *
+  FROM  TutorialAppSchema.UserSalary;
 
 UPDATE  UserSalary
-   SET  UserSalary.AvgForDepartment = AvgSalaryInDepartment.AvgSalaryInDepartment
-        , UserSalary.AvgForJobTitle = AvgSalaryForJobTitle.AvgSalaryForJobTitle
-  FROM  TutorialAppSchema.UserSalary
-      JOIN TutorialAppSchema.UserJobInfo AS UJI
-          ON UJI.UserId = UserSalary.UserId
-      OUTER APPLY (
-                      SELECT    UJI2.Department
-                                , AVG (UserSalary.Salary) AS AvgSalaryInDepartment
-                        FROM    TutorialAppSchema.Users
-                            JOIN TutorialAppSchema.UserJobInfo UJI2
-                                ON UJI2.UserId = Users.UserId
-                            JOIN TutorialAppSchema.UserSalary
-                                ON UserSalary.UserId = Users.UserId
-                       WHERE UJI2.Department = UJI.Department
-                       GROUP BY UJI2.Department
-                  ) AS AvgSalaryInDepartment
-      OUTER APPLY (
-                      SELECT    UJI2.JobTitle
-                                , AVG (UserSalary.Salary) AS AvgSalaryForJobTitle
-                        FROM    TutorialAppSchema.Users
-                            JOIN TutorialAppSchema.UserJobInfo UJI2
-                                ON UJI2.UserId = Users.UserId
-                            JOIN TutorialAppSchema.UserSalary
-                                ON UserSalary.UserId = Users.UserId
-                       WHERE UJI2.JobTitle = UJI.JobTitle
-                       GROUP BY UJI2.JobTitle
-                  ) AS AvgSalaryForJobTitle;
+   SET  UserSalary.AvgSalary = DepartmentAverage.AvgSalary
+  FROM  TutorialAppSchema.UserSalary AS UserSalary
+      LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+          ON UserJobInfo.UserId = UserSalary.UserId
+      CROSS APPLY ( -- Similar to JOIN
+                      -- SELECT TOP 1 
+                      SELECT    ISNULL (UserJobInfo2.Department, 'No Department Listed') AS Deparment
+                                , AVG (UserSalary2.Salary) AS AvgSalary
+                        FROM    TutorialAppSchema.UserSalary AS UserSalary2
+                            LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo2
+                                ON UserJobInfo2.UserId = UserSalary2.UserId
+                       WHERE ISNULL (UserJobInfo2.Department, 'No Department Listed') = ISNULL (UserJobInfo.Department, 'No Department Listed')
+                       GROUP BY UserJobInfo2.Department
+                  ) AS DepartmentAverage;
+
+SELECT  Users.UserId
+        , Users.FirstName + ' ' + Users.LastName AS FullName
+        , UserJobInfo.JobTitle
+        , UserJobInfo.Department
+        , UserSalary.AvgSalary
+        , UserSalary.Salary
+        , Users.Email
+        , Users.Gender
+        , Users.Active
+  FROM  TutorialAppSchema.Users AS Users
+      --INNER JOIN
+      JOIN TutorialAppSchema.UserSalary AS UserSalary
+          ON UserSalary.UserId = Users.UserId
+      LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+          ON UserJobInfo.UserId = Users.UserId
+ WHERE  Users.Active = 1
+ ORDER BY Users.UserId DESC;
