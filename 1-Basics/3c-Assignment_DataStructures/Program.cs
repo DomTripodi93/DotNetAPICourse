1486f09917aa72edd7f625ffe532609db7112a81
Dominic Tripodi <dtripodi93@gmail.com>
2023-05-30 08:27:35 -0400

    Updated Assignment Answer for Data Structures

---
+++ 1-Basics/3c-Assignment_DataStructures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write Your Code Here
+            
+            Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>() {
+                {"cheese", 5.99m},
+                {"carrots", 2.99m}
+            };
+            
+            //Write You Code Above This Line
+            Console.WriteLine(itemPrices["cheese"].GetType());
+            Console.WriteLine(itemPrices["cheese"]);
+            Console.WriteLine(itemPrices["carrots"]);
+        }
+    }
+}
